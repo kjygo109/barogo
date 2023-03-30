2e863b7f8ebbdad5e5e59eac4317be28b8c3c977
jaeYoung <kjygo109@naver.com>
2023-03-31 00:04:32 +0900

    security modify(h2-console setting)

--- src/main/java/com/example/barogo/config/SpringSecurityConfig.java
+++ src/main/java/com/example/barogo/config/SpringSecurityConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -25,20 +25,23 @@ public class SpringSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http.csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")).disable()
-      .authorizeHttpRequests().requestMatchers( "/member/**").permitAll()
-      .anyRequest().authenticated()
-      .and()
-      .headers()
-      .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
-      .and()
-      .addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+    http.csrf().disable()
+            .authorizeHttpRequests()
+            .requestMatchers("/member/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
 
   @Bean
-  public PasswordEncoder passwordEncoder(){
+  public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
   }
 }
